7afe7905cd2a4f2f49437d10c368238e0ac96755
Yanuar Adi <yanuaradinandra23@gmail.com>
2025-12-11 10:40:52 +0800

    update

--- main.tsx
+++ main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
@@ -13,3 +14,6 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+
+
